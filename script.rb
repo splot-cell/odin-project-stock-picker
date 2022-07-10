f056954b68bf2e5f8dee82ab158c1e563e960524
Olly Seber <olly_s@hotmail.co.uk>
2022-07-10 08:19:20 -0700

    Find maximum price_difference from hash array
    Updated create_price_hash to return an array of hashes instead of a hash
    of hashes.
    
    Updated stock_picker method to find the maximum price_difference from
    this array of hashes.

--- script.rb
+++ script.rb
@@ -1,14 +1,14 @@
 def stock_picker(prices)
-    puts create_price_hash(prices)
-    
+    price_hash = create_price_hash(prices)
+    max_profit = price_hash.max { |a, b| a[:price_difference] <=> b[:price_difference]}
+    puts max_profit
 end
 
 def create_price_hash(prices)
-    price_difference = {}
+    price_difference = []
     prices.each_with_index do |buy_price, buy_index|
         for sell_index in buy_index + 1 .. prices.length - 1
-            key = "#{buy_index}, #{sell_index}"
-            price_difference[key] = {buy_index: buy_index, sell_index: sell_index, price_difference: prices[sell_index] - buy_price}
+            price_difference.push({buy_index: buy_index, sell_index: sell_index, price_difference: prices[sell_index] - buy_price})
         end
     end
     price_difference
