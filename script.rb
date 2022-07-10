8dc2c35d457425c561a5bf3e81d6f9258e58632a
Olly Seber <olly_s@hotmail.co.uk>
2022-07-10 08:12:12 -0700

    Add create_price_hash method
    Method takes prices array and creates a hash of hashes containing the
    price difference between each pair of prices, and the indexes of those
    prices.

--- script.rb
+++ script.rb
@@ -1,9 +1,17 @@
 def stock_picker(prices)
+    puts create_price_hash(prices)
+    
+end
+
+def create_price_hash(prices)
+    price_difference = {}
     prices.each_with_index do |buy_price, buy_index|
         for sell_index in buy_index + 1 .. prices.length - 1
-            puts "#{buy_index}: #{buy_price} -> #{sell_index}: #{prices[sell_index]}"
+            key = "#{buy_index}, #{sell_index}"
+            price_difference[key] = {buy_index: buy_index, sell_index: sell_index, price_difference: prices[sell_index] - buy_price}
         end
     end
+    price_difference
 end
 
 stock_picker([17,3,6,9,15,8,6,1,10])
